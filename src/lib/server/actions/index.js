8289874274b2bfd779fa9de5ad952f7a644e2f17
Feodor Rusanov <f.rusanov@gmail.com>
2024-07-18 13:21:40 +0300

    fix: getSettings

--- src/lib/server/actions/index.js
+++ src/lib/server/actions/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   getCert: require('./getCert'),
   getInfo: require('./getInfo'),
+  getSettings: require('./getSettings'),
   // updateSettings: require('./updateSettings'),
   updateCerts: require('./updateCerts'),
   deleteCert: require('./deleteCert')
